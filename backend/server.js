a9c7fa1b63267675effe68ce50f311b8a1108f31
Sehar-Tahir <sehartahirofficial@gmail.com>
2026-06-21 20:53:22 +0500

    url

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // ====================== CORS FIX ======================
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://karachi-weather.vercel.app/'], // Add your frontend ports
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://karachi-weather.vercel.app'], // Add your frontend ports
   credentials: true,           // Important for cookies & auth
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
